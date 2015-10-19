9c76fde416ab3de1e46f36ebbe4888ce6cede5f7
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2015-10-19 18:44:19 -0400

    R package

--- vim-plugins-profile.sh
+++ vim-plugins-profile.sh
@@ -21,12 +21,20 @@ paste tmp1.log tmp2.log |sed 's/\s\+/,/g' > profile.csv
 rm tmp.log tmp1.log tmp2.log
 
 echo "Crunching data and generating profile plot ..."
+
+# Check if R is available
+echo " "
+type R > /dev/null 2>&1 || { echo >&2 "Package R is required but it's not installed. \nPlease install R using your package manager, \nor check out cran.r-project.org for instructions. \nAborting."; exit 1; }
+
+# Still here? Great! Let's move on!
+# Let's do the R magic!
 R --vanilla --quiet --slave --file="vim-plugins-profile-plot.R"
 #R -q -f "vim-plugins-profile-plot.R" > /dev/null 2>&1
 
+# we use result.csv, which is saved from R
+# delete profile.csv since it is used to feed into R
 rm profile.csv
 
-# we use result.csv, which is saved from R
 
 echo " "
 echo 'Your plugins startup profile graph is saved '
@@ -35,7 +43,7 @@ echo " "
 echo "=========================================="
 echo "Top 10 Plugins That Slows Down Vim Startup"
 echo "=========================================="
-cat -n results.csv |head -n 10
+cat -n results.csv |head -n 10 # change this 10 to see more in this `Top List`
 echo "=========================================="
 
 echo "Done!"
