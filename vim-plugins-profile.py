e51d68722b0291baa16d48651a82a75e96aa1c31
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2017-03-01 18:36:12 -0500

    rename

--- vim-plugins-profile.py
+++ vim-plugins-profile.py
@@ -3,6 +3,7 @@
 #
 # vim-profiler - Utility script to profile (n)vim (e.g. startup)
 # Copyright © 2015 Benjamin Chrétien
+# Copyright © 2017 Hörmet Yiltiz <hyiltiz@github.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
